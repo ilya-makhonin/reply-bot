5d5a4ef22b9c568374daf66d7c8dda436ad19f87
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-18 01:58:08 +0300

    Update code...

--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
-const bot = require('node-telegram-bot-api');
-const { token } = require('./config');
+const express = require('express');
+const { server_pkey, server_cert, server_list, server_port, server_host } = require('./config');
