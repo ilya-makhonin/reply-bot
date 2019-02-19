dee172073405f153363984f93fad8031c0a2c6b9
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-19 21:54:38 +0300

    Created simple bot

--- src/bot.js
+++ src/bot.js
@@ -1,2 +1,24 @@
-const bot = require('node-telegram-bot-api');
-const { token } = require('./config');
+const TelegramBot = require('node-telegram-bot-api');
+const { start, help, answer } = require('./variables');
+const { admins, token, chat } = require('./config');
+
+let bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, start, {
+    parse_mode: 'markdown'
+  });
+});
+
+bot.onText(/\/help/, (msg) => {
+  bot.sendMessage(msg.chat.id, help, {
+    parse_mode: 'markdown'
+  });
+});
+
+bot.on('message', (msg) => {
+  bot.forwardMessage(chat, msg.chat.id, msg.message_id);
+  bot.sendMessage(msg.chat.id, answer, {
+    parse_mode: 'markdown'
+  })
+});
