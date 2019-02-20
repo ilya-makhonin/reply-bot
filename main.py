a0fa1bff725316b47f4a9916aa565d9e46a1b564
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-20 23:28:02 +0300

    Created function for starting server

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import cherrypy
-import logging
 import telebot
 import time
 from source.config import *
+from source.bot import initial_bot
 
 
 WEB_HOOK_URL_BASE = "https://%s:%s" % (WEB_HOOK_HOST, WEB_HOOK_PORT)
 WEB_HOOK_URL_PATH = "/%s/" % (TOKEN, )
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)
-
-bot = telebot.TeleBot(TOKEN)
+BOT = initial_bot()
 
 
 class WebHookServer(object):
@@ -26,30 +22,19 @@ class WebHookServer(object):
             length = int(cherrypy.request.headers['content-length'])
             json_string = cherrypy.request.body.read(length).decode("utf-8")
             update = telebot.types.Update.de_json(json_string)
-            bot.process_new_updates([update])
+            BOT.process_new_updates([update])
             return ''
         else:
             raise cherrypy.HTTPError(403)
 
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hi there, I am EchoBot.\nI am here to echo your kind words back to you.")
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
 def main():
-    bot.remove_webhook()
+    BOT.remove_webhook()
     time.sleep(1)
-    bot.set_webhook(url=WEB_HOOK_URL_BASE + WEB_HOOK_URL_PATH, certificate=open(WEB_HOOK_SSL_CERT, 'r'))
+    BOT.set_webhook(url=WEB_HOOK_URL_BASE + WEB_HOOK_URL_PATH, certificate=open(WEB_HOOK_SSL_CERT, 'r'))
     access_log = cherrypy.log.access_log
     for handler in tuple(access_log.handlers):
         access_log.removeHandler(handler)
-
     cherrypy.config.update({
         'server.socket_host': WEB_HOOK_LISTEN,
         'server.socket_port': WEB_HOOK_PORT,
@@ -57,7 +42,6 @@ def main():
         'server.ssl_certificate': WEB_HOOK_SSL_CERT,
         'server.ssl_private_key': WEB_HOOK_SSL_PRIV
     })
-
     cherrypy.quickstart(WebHookServer(), WEB_HOOK_URL_PATH, {'/': {}})
 
 
