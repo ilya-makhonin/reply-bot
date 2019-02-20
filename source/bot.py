327ee6466b0e14779f743e998f8950a1032728c7
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-20 23:29:30 +0300

    Wrote simple bot

--- source/bot.py
+++ source/bot.py
@@ -1,1 +1,25 @@
-from telebot import TeleBot, types
+import telebot
+import logging
+from source.config import TOKEN, CHAT
+from source.variables import *
+
+
+def initial_bot(use_logging=True, level_name='DEBUG'):
+    bot = telebot.TeleBot(TOKEN)
+
+    @bot.message_handler(commands=['start'])
+    def start_handler(message: telebot.types.Message):
+        bot.send_message(message.from_user.id, start_mess)
+
+    @bot.message_handler(commands=['help'])
+    def help_handler(message: telebot.types.Message):
+        bot.send_message(message.from_user.id, help_mess)
+
+    @bot.message_handler(lambda message: True)
+    def forward_handler(message: telebot.types.Message):
+        bot.forward_message(CHAT, message.chat.id, message.message_id)
+
+    if use_logging:
+        logger = telebot.logger
+        telebot.logger.setLevel(logging.getLevelName(level_name))
+    return bot
