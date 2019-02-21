fd2352d70722cabf2b5f117f4f9622e5a2d0528c
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-22 01:54:51 +0300

    Added logging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import cherrypy
 import telebot
 import time
+from source.log import log
 from source.config import *
 from source.bot import initial_bot
 
@@ -11,6 +12,7 @@ WEB_HOOK_URL_BASE = "https://%s:%s" % (WEB_HOOK_HOST, WEB_HOOK_PORT)
 WEB_HOOK_URL_PATH = "/%s/" % (TOKEN, )
 
 BOT = initial_bot()
+server_logger = log('server', 'server.log')
 
 
 class WebHookServer(object):
@@ -23,15 +25,19 @@ class WebHookServer(object):
             json_string = cherrypy.request.body.read(length).decode("utf-8")
             update = telebot.types.Update.de_json(json_string)
             BOT.process_new_updates([update])
+            server_logger.debug(f"New updates. Info: {[update]}")
             return ''
         else:
+            server_logger.debug(f"Server error! Error status 403")
             raise cherrypy.HTTPError(403)
 
 
 def main():
+    server_logger.debug("Start main function")
     BOT.remove_webhook()
     time.sleep(1)
     BOT.set_webhook(url=WEB_HOOK_URL_BASE + WEB_HOOK_URL_PATH, certificate=open(WEB_HOOK_SSL_CERT, 'r'))
+    server_logger.debug("Web hook has been set success")
     access_log = cherrypy.log.access_log
     for handler in tuple(access_log.handlers):
         access_log.removeHandler(handler)
@@ -43,7 +49,12 @@ def main():
         'server.ssl_private_key': WEB_HOOK_SSL_PRIV
     })
     cherrypy.quickstart(WebHookServer(), WEB_HOOK_URL_PATH, {'/': {}})
+    server_logger.debug("Server is started success")
 
 
 if __name__ == '__main__':
-    main()
+    server_logger.debug("Bot is starting")
+    try:
+        main()
+    except Exception as error:
+        server_logger.debug(f"Error in main function. Info: {error}")
