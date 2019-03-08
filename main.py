88344fb978e8a9fcef115b3c675a533f73f1ead8
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-09 01:18:56 +0300

    Changed loggin level from debug to info, updated to create bot instance

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from source.bot import initial_bot
 WEB_HOOK_URL_BASE = "https://%s:%s" % (WEB_HOOK_HOST, WEB_HOOK_PORT)
 WEB_HOOK_URL_PATH = "/%s/" % (TOKEN, )
 
-BOT = initial_bot()
-server_logger = log('server', 'server.log')
+BOT = initial_bot(use_logging=True, level_name='INFO')
+server_logger = log('server', 'server.log', 'INFO')
 
 
 class WebHookServer(object):
@@ -25,19 +25,19 @@ class WebHookServer(object):
             json_string = cherrypy.request.body.read(length).decode("utf-8")
             update = telebot.types.Update.de_json(json_string)
             BOT.process_new_updates([update])
-            server_logger.debug(f"New updates. Info: {[update]}")
+            server_logger.info(f"New updates. Info: {[update]}")
             return ''
         else:
-            server_logger.debug(f"Server error! Error status 403")
+            server_logger.info(f"Server error! Error status 403")
             raise cherrypy.HTTPError(403)
 
 
 def main():
-    server_logger.debug("Start main function")
+    server_logger.info("Start main function")
     BOT.remove_webhook()
     time.sleep(1)
     BOT.set_webhook(url=WEB_HOOK_URL_BASE + WEB_HOOK_URL_PATH, certificate=open(WEB_HOOK_SSL_CERT, 'r'))
-    server_logger.debug("Web hook has been set success")
+    server_logger.info("Web hook has been set success")
     access_log = cherrypy.log.access_log
     for handler in tuple(access_log.handlers):
         access_log.removeHandler(handler)
@@ -48,13 +48,13 @@ def main():
         'server.ssl_certificate': WEB_HOOK_SSL_CERT,
         'server.ssl_private_key': WEB_HOOK_SSL_PRIV
     })
-    server_logger.debug("Server is started success")
+    server_logger.info("Server is started success")
     cherrypy.quickstart(WebHookServer(), WEB_HOOK_URL_PATH, {'/': {}})
 
 
 if __name__ == '__main__':
-    server_logger.debug("Bot is starting")
+    server_logger.info("Bot is starting")
     try:
         main()
     except Exception as error:
-        server_logger.debug(f"Error in main function. Info: {error.with_traceback(None)}")
+        server_logger.info(f"Error in main function. Info: {error.with_traceback(None)}")
