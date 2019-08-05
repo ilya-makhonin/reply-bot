619ff09c61beb7bade5e88f0e1a1cf892ac33078
Ilia Makhonin <noizemkutane77@gmail.com>
2019-08-05 17:51:22 +0300

    Deleted the method for printing some development info in terminal

--- src/forward.py
+++ src/forward.py
@@ -12,16 +12,6 @@ class Forward:
         self.__develop_mode = develop_mode
         self.logger = log('forward', 'forward.log', 'INFO')
 
-    def develop_debugging(self, method: str, info: str):
-        """
-        :param method: <str> - a mane of a method's this class
-        :param info: <str> - information about changes
-        :return: nothing
-        """
-        print('Method: ', method)
-        print('Info about this process: ', info)
-        print('Global state now: ', self.message_forward_data)
-
     def add_key(self, message: types.Message):
         """
         Method for adding information about a user to app cache
@@ -34,10 +24,8 @@ class Forward:
             'first': message.from_user.first_name,
             'last': message.from_user.last_name
         }
-        # I have to find method is better
+        # TODO: I have to find method is better
         self.message_forward_data[message.date] = new_key
-        if self.__develop_mode:
-            self.develop_debugging('add_key', f"Adding new key {message.date} => {str(new_key)}")
         self.logger.info(f"Method add_key. Information: {message.date} => {message.from_user.id}")
 
     def get_id(self, message: types.Message):
@@ -48,8 +36,6 @@ class Forward:
         """
         try:
             result: dict = self.message_forward_data.get(message.reply_to_message.forward_date)
-            if self.__develop_mode:
-                self.develop_debugging('get_id', f"Getting id from {str(result)}")
             self.logger.info(f"Method get_id. Information: {result}")
             return result.get('id')
         except Exception as error:
@@ -63,10 +49,6 @@ class Forward:
         """
         try:
             result = self.message_forward_data.pop(message.reply_to_message.forward_date)
-            if self.__develop_mode:
-                self.develop_debugging(
-                    'deleted_data',
-                    f"Deleting data about a user by date: {str(message.reply_to_message.date)} => {str(result)}")
             self.logger.info(f"Method delete_data. "
                              f"Information: user id {result} was deleted by {message.reply_to_message.date}")
         except Exception as error:
@@ -78,8 +60,6 @@ class Forward:
         :return: nothing
         """
         self.message_forward_data.clear()
-        if self.__develop_mode:
-            self.develop_debugging('clear_data', 'Global state is empty')
         self.logger.info(f"Method clear_data. Information: self.message_forward_data had cleared")
 
     def get_mode(self):
